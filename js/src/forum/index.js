bee2a60189fbaff5178ff6f7c049238a88d14980
William Di Cicco <nearata@protonmail.com>
2020-10-23 22:38:57 +0200

    Update to Beta 14

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,24 +4,21 @@ import CommentPost from 'flarum/components/CommentPost';
 import copyTextToClipboard from 'copy-text-to-clipboard';
 
 app.initializers.add('nearata/flarum-ext-copy-code-to-clipboard', () => {
-    extend(CommentPost.prototype, 'config', function(res, isInitialized) {
-        if (isInitialized) {
-            return;
-        }
-
+    extend(CommentPost.prototype, 'oncreate', function () {
         const elements = this.element.getElementsByTagName('pre');
         for (const el of elements) {
-            const copyElement = document.createElement('div');
+            const copyElement = document.createElement('span');
             copyElement.textContent = app.translator.trans('nearata-copy-code-to-clipboard.forum.copy');
-            copyElement.classList.add('copy-code')
+            copyElement.classList.add('nearata-copy-code');
+            copyElement.style.color = app.forum.data.attributes.themePrimaryColor;
 
-            el.appendChild(copyElement);
+            el.after(copyElement);
         }
 
-        const copyCodeElements = this.element.getElementsByClassName('copy-code');
+        const copyCodeElements = this.element.getElementsByClassName('nearata-copy-code');
         for (const el of copyCodeElements) {
             el.addEventListener('click', () => {
-                const childNodes = el.parentElement.childNodes;
+                const childNodes = el.previousSibling.childNodes;
 
                 for (const n of childNodes) {
                     const nodeName = n.nodeName.toLowerCase();
