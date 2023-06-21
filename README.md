74810f801f7a35cb53ae6f26b087ecf1cbab7796
William <nearata@protonmail.com>
2023-06-21 15:37:28 +0200

    update dependencies

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Copy Code to Clipboard
 
-> A [Flarum](https://flarum.org) extension that allows the user to easly copy the text inside a code tag.
+[![license](https://img.shields.io/github/license/nearata/flarum-ext-copy-code-to-clipboard?style=flat)](https://github.com/Nearata/flarum-ext-copy-code-to-clipboard/blob/main/UNLICENSE)
+[![packagist](https://img.shields.io/packagist/v/nearata/flarum-ext-copy-code-to-clipboard?style=flat)](https://packagist.org/packages/nearata/flarum-ext-copy-code-to-clipboard)
+[![changelog](https://img.shields.io/github/release-date/nearata/flarum-ext-copy-code-to-clipboard?label=last%20release%20date)](https://github.com/Nearata/flarum-ext-copy-code-to-clipboard/blob/main/CHANGELOG.md)
+
+> Allows the user to easly copy the text inside a code block.
 
 ![Imgur](https://i.imgur.com/GmuzExa.png)
 
 ## Install
 
 ```sh
-composer require nearata/flarum-ext-copy-code-to-clipboard
+composer require nearata/flarum-ext-copy-code-to-clipboard:"*"
+```
+
+## Update
+
+```sh
+composer update nearata/flarum-ext-copy-code-to-clipboard:"*"
 ```
 
 ## Remove
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,40 +1,49 @@
-import { extend } from 'flarum/common/extend';
-import app from 'flarum/forum/app';
-import CommentPost from 'flarum/forum/components/CommentPost';
+import { extend } from "flarum/common/extend";
+import app from "flarum/forum/app";
+import CommentPost from "flarum/forum/components/CommentPost";
 
-app.initializers.add('nearata-copy-code-to-clipboard', () => {
-    extend(CommentPost.prototype, 'oncreate', function () {
-        for (const el of this.element.querySelectorAll('pre')) {
-            const copy = document.createElement('button');
+app.initializers.add("nearata-copy-code-to-clipboard", () => {
+  extend(CommentPost.prototype, "oncreate", function () {
+    for (const el of this.element.querySelectorAll("pre")) {
+      const copy = document.createElement("button");
 
-            copy.classList.add('nearata-copy-code');
-            copy.setAttribute('title', app.translator.trans('nearata-copy-code-to-clipboard.forum.copy'));
-            copy.style.color = app.forum.attribute('themePrimaryColor');
-            copy.style.borderColor = app.forum.attribute('themePrimaryColor');
+      copy.classList.add("nearata-copy-code");
+      copy.setAttribute(
+        "title",
+        app.translator.trans("nearata-copy-code-to-clipboard.forum.copy")
+      );
+      copy.style.color = app.forum.attribute("themePrimaryColor");
+      copy.style.borderColor = app.forum.attribute("themePrimaryColor");
 
-            const copyIcon = document.createElement('i');
-            copyIcon.classList.add('far', 'fa-clipboard');
+      const copyIcon = document.createElement("i");
+      copyIcon.classList.add("far", "fa-clipboard");
 
-            copy.append(copyIcon)
-            el.prepend(copy);
-        }
+      copy.append(copyIcon);
+      el.prepend(copy);
+    }
 
-        for (const el of this.element.querySelectorAll('.nearata-copy-code')) {
-            el.addEventListener('click', function (_) {
-                navigator.clipboard.writeText(el.parentNode.querySelector('code').textContent);
+    for (const el of this.element.querySelectorAll(".nearata-copy-code")) {
+      el.addEventListener(
+        "click",
+        function (_) {
+          navigator.clipboard.writeText(
+            el.parentNode.querySelector("code").textContent
+          );
 
-                const icon = el.querySelector('i');
+          const icon = el.querySelector("i");
 
-                icon.classList.replace('far', 'fas');
-                icon.classList.replace('fa-clipboard', 'fa-check');
+          icon.classList.replace("far", "fas");
+          icon.classList.replace("fa-clipboard", "fa-check");
 
-                setTimeout(function () {
-                    icon.classList.replace('fas', 'far');
-                    icon.classList.replace('fa-check', 'fa-clipboard');
+          setTimeout(function () {
+            icon.classList.replace("fas", "far");
+            icon.classList.replace("fa-check", "fa-clipboard");
 
-                    clearTimeout(0);
-                }, 1000);
-            }, false)
-        }
-    });
+            clearTimeout(0);
+          }, 1000);
+        },
+        false
+      );
+    }
+  });
 });
